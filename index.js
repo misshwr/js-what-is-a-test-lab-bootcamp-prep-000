6a65ed3e799d2c65961d18790305020f7ba88168
Manal HW <mnleen@gmail.com>
2019-06-18 11:25:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Susan"
+var Name = "Susan"
 var height = 35
-var message = ` is ${height} inches taller than ${name}`
+var message = ` is ${height} inches taller than ${Name}`
 
-module.exports = {name, height, message}
+module.exports = {Name, height, message}
